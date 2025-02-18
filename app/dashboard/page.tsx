cce7c443a1ab40b1fc00ce28c170d50210b84532
unknown <bomerenma@gmail.com>
2025-02-18 16:00:34 +0100

    second commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import Image from "next/image";
 
 interface UserData {
   id: string;
